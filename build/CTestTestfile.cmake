5446f34e0bc3357443c3ff83abad375f79389e02
TaiHo <TaiAxolotlHo735175@gmail.com>
2026-06-24 19:16:16 +0700

    set up intial plan

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/TaiAx/Desktop/Lucid-Game-Engine
-# Build directory: C:/Users/TaiAx/Desktop/Lucid-Game-Engine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("externals/glfw")
-subdirs("externals/luc_compiler")
